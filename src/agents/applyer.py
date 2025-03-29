349aee502b428daec6a50b06ef6669e6d6992910
openfinesse <umerak@pm.me>
2025-03-29 19:12:17 -0400

    Added upload functionality

--- src/agents/applyer.py
+++ src/agents/applyer.py
@@ -27,7 +27,7 @@ controller = Controller()
 
 logger = logging.getLogger(__name__)
 
-resume = Path.cwd() / 'UmerKhan_Resume.pdf'
+resume = Path.cwd() / 'data/reference_resume/UmerKhan_Resume.pdf'
 
 if not resume.exists():
 	raise FileNotFoundError(f'You need to set the path to your resume file in the resume variable. Resume file not found at {resume}')
@@ -37,10 +37,10 @@ class Job(BaseModel):
     company: str
     location: Optional[str] = None
     fit_score: float
-    estimated_salary: str
+    salary: str
     link: str
 
-@controller.action('Read my resume for context to generate fit scores, estimate salaries, and fill forms')
+@controller.action('Read my resume for context to fill forms, and help generate fit scores and estimate salaries')
 def read_resume():
 	pdf = PdfReader(resume)
 	text = ''
@@ -49,26 +49,53 @@ def read_resume():
 	logger.info(f'Read resume with {len(text)} characters')
 	return ActionResult(extracted_content=text, include_in_memory=True)
 
-@controller.action('Generate a fit score for the job based on the job description and my resume')
-def generate_fit_score(job: Job):
-    return f"Fit score: {job.fit_score}"
+@controller.action('Save jobs to file - with a score of how well it fits me, and the estimated or extracted salary', param_model=Job)
+def save_jobs(job: Job):
+	with open('jobs.csv', 'a', newline='') as f:
+		writer = csv.writer(f)
+		writer.writerow([job.title, job.company, job.link, job.salary, job.location, job.fit_score])
 
-@controller.action('Estimate the salary for the job based on the job description, location, trends, and company')
-def estimate_salary(job: Job):
-    return f"Estimated salary: {job.estimated_salary}"
-
-@controller.action('Save job data to CSV', param_model=Job)
-def write_to_csv(job: Job):
-    with open('jobs.csv', 'a', newline='') as f:
-        writer = csv.writer(f)
-        writer.writerow([job.title, job.company, job.location, job.fit_score, job.estimated_salary, job.link])
-
-    return 'Saved job to file'
+	return 'Saved job to file'
 
 @controller.action('Read jobs from CSV file')
 def read_jobs():
 	with open('jobs.csv', 'r') as f:
 		return f.read()
+     
+@controller.action('Upload resume or cover letter to element with file path',)
+async def upload_file(index: int, path: str, browser: BrowserContext, available_file_paths: list[str]):
+	if path not in available_file_paths:
+		return ActionResult(error=f'File path {path} is not available')
+
+	if not os.path.exists(path):
+		return ActionResult(error=f'File {path} does not exist')
+
+	dom_el = await browser.get_dom_element_by_index(index)
+
+	file_upload_dom_el = dom_el.get_file_upload_element()
+
+	if file_upload_dom_el is None:
+		msg = f'No file upload element found at index {index}'
+		logger.info(msg)
+		return ActionResult(error=msg)
+
+	file_upload_el = await browser.get_locate_element(file_upload_dom_el)
+
+	if file_upload_el is None:
+		msg = f'No file upload element found at index {index}'
+		logger.info(msg)
+		return ActionResult(error=msg)
+
+	try:
+		await file_upload_el.set_input_files(path)
+		msg = f'Successfully uploaded file to index {index}'
+		logger.info(msg)
+		return ActionResult(extracted_content=msg, include_in_memory=True)
+	except Exception as e:
+		msg = f'Failed to upload file to index {index}: {str(e)}'
+		logger.info(msg)
+		return ActionResult(error=msg)
+
 
 @controller.action('Ask user for further information or help')
 def ask_human(question: str):
@@ -85,83 +112,48 @@ cdp_url = f"wss://connect.steel.dev/?apiKey={STEEL_API_KEY}&sessionId={session.i
 browser = Browser(config=BrowserConfig(cdp_url=cdp_url))
 browser_context = BrowserContext(browser=browser)
 
-# Initialize the LLMs
+# Initialize LLM
 model = ChatOpenAI(
     model="gpt-4o",
     temperature=0.4,
     api_key=OPENAI_API_KEY
 )
 
-planner_model = ChatOpenAI(
-    model="o3-mini",
-    temperature=0.65,
-    api_key=OPENAI_API_KEY
-)
+# Open the linkedin jobs page
+# goto_linkedin = [
+#     {'go_to_url': {'url': 'https://www.linkedin.com/jobs/search/?f_TPR=r86400&geoId=90009551&keywords=((%22Systems%22%20OR%20%22System%22%20OR%20%22Desktop%22%20OR%20%22Technical%22%20OR%20%22Technology%22%20OR%20%22IT%22%20OR%20%22Endpoint%22%20OR%20%22Help%20Desk%22)%20AND%20(%22Support%22%20OR%20%22Engineer%22%20OR%20%22Analyst%22%20OR%20%22Specialist%22%20OR%20%22Administrator%22%20OR%20%22Technician%22))%20NOT%20(%22Data%20System%22%20OR%20%22Power%20Systems%22%20OR%20%22Business%22%20OR%20%22Bilingual%22%20OR%20%22Software%22)&origin=JOB_SEARCH_PAGE_JOB_FILTER&refresh=true'}},
+# ]
 
-login_creds = {'x_username': f'{LINKEDIN_USERNAME}', 'x_password': f'{LINKEDIN_PASSWORD}'}
-
-# Define tasks
-login_task = "Goto https://www.linkedin.com/login and login using x_username and x_password"
-
-
-# Define the initial standard actions
-goto_linkedin = [
-    {'open_tab': {'url': 'https://www.linkedin.com/jobs/search/?f_TPR=r86400&geoId=90009551&keywords=((%22Systems%22%20OR%20%22System%22%20OR%20%22Desktop%22%20OR%20%22Technical%22%20OR%20%22Technology%22%20OR%20%22IT%22%20OR%20%22Endpoint%22%20OR%20%22Help%20Desk%22)%20AND%20(%22Support%22%20OR%20%22Engineer%22%20OR%20%22Analyst%22%20OR%20%22Specialist%22%20OR%20%22Administrator%22%20OR%20%22Technician%22))%20NOT%20(%22Data%20System%22%20OR%20%22Power%20Systems%22%20OR%20%22Business%22%20OR%20%22Bilingual%22%20OR%20%22Software%22)&origin=JOB_SEARCH_PAGE_JOB_FILTER&refresh=true'}},
+upload_test = [
+    {'go_to_url': {'url': 'https://www.csm-testcenter.org/test?do=show&subdo=common&test=file_upload'}},
 ]
 
-extract_jobs = [
-          {'extract_content': {'goal': 'Extract the following information for all the search results: job titles, company names, locations, and cooresponding links, param_model=Job', 'browser': 'browser', 'page_extraction_llm': 'gpt-4o'}}
-]
-
-# Configure AI agents
-login_agent = Agent(
-    task=login_task,
-    llm=model,
-    controller=controller,
-    # planner_llm=planner_model,
-    sensitive_data=login_creds,
-    # initial_actions=goto_linkedin,
-    browser=browser,
-    browser_context=browser_context,
+tasks = (
+    'Read my resume for context'
+    'Read jobs from CSV file'
+    'Start applying to the jobs'
 )
 
+# Configure AI agent
 agent = Agent(
-    task="",
+    task=tasks,
     llm=model,
-    planner_llm=planner_model,
-    initial_actions=extract_jobs,
-    # message_context="",
+    initial_actions=upload_test,
     controller=controller,
     use_vision=True,
     browser=browser,
     browser_context=browser_context,
+	available_file_paths=[str(resume.absolute())]
 )
-
-# next_agent = Agent(
-#     task="Filter the results using the following criteria: Date posted in the last 24 hours, Location: Greater Toronto Area, Ontario, Canada",
-#     llm=model,
-#     browser=browser,
-#     browser_context=browser_context
-# )
 
 # Main function to run the agent
 async def main():
     try:
-        # Run the agent
-        print("Starting login agent...")
-        await login_agent.run()
-        print("Login agent completed successfully.")
-
+        print("Starting job application agent...")
+        await agent.run()
+        print("Job application agent completed successfully.")
     except Exception as e:
-        # Print errors
         print(f"An error occurred: {e}")
-
-    # try:
-    #     print("Starting job search agent...")
-    #     await agent.run()
-    #     print("Job search agent completed successfully.")
-    # except Exception as e:
-    #     print(f"An error occurred: {e}")
 
     finally:
         # Close the browser and release the session
